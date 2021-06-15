93b5800913a683c76a938bf4e71e6ebeb5938837
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-15 00:43:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//conect "Guess" button to "random" function
+document.getElementById('button').addEventListener('click',randomizer)
+let randomNumber = 0
+let userGuess = 0
+
+function randomizer() {
+  //get number typed in by user and covert to an integer
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
+
+  //generate random number between 1 and 6
+  randomNumber = Math.random() * 6 + 1
+  randomNumber = parseInt(randomNumber)
+  //compare user's geuss with random number
+  if(userGuess == randomNumber) {
+    document.getElementById('answer').innerHTML = "Correct!"
+  }
+}
